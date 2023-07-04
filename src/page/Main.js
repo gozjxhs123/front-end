c74e2db6c08781258190d23ac77657448bdcc12b
eunseong7207 <les070207@naver.com>
2023-07-04 21:47:59 +0900

    .

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,9 +1,40 @@
+import styled from "styled-components"
+import FirstSection from "../components/OnBoarding/Section/FirstSection"
+import SecondSection from "../components/OnBoarding/Section/SecondSection"
+import Header from "../components/header/Header"
+import Footer from "../components/footer/footer"
+
 const Main = () => {
     return (
-        <div>
-            hello world
-        </div>
+        <OnBoardingContainer>
+            <OnBoardingWrapper>
+                <OnBoardingSectionContainer>
+                    <Header />
+                    <FirstSection />
+                    <SecondSection />
+                    <Footer />
+                </OnBoardingSectionContainer>
+            </OnBoardingWrapper>
+        </OnBoardingContainer>
     )
 }
 
+const OnBoardingContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+`
+
+const OnBoardingWrapper = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+`
+
+const OnBoardingSectionContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`
 export default Main
