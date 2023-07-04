3e43c15be2506a5bb6584ad10c0cdf73fb9f9fcb
eunseong7207 <les070207@naver.com>
2023-07-04 19:09:25 +0900

    perf: file split

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
-import CAI from "../assets/CAI.png"
+import FirstSection from "../components/OnBoarding/Section/FistSection"
+import SecondSection from "../components/OnBoarding/Section/SecondSection"
 
 const Main = () => {
     return (
@@ -8,37 +8,8 @@ const Main = () => {
             <OnBoardingWrapper>
                 <OnBoardingSectionContainer>
                     <Hs style={{ height: "6.2vh" }}></Hs>
-                    <FirstSection>
-                        <LeftFirstSection>
-                            <Title>Ai로 당신의 삶을 안전하게</Title>
-                            <Text>
-                                <div>
-                                    '여러분의 안전을 지키는 새로운 차원을
-                                    경험해보세요.
-                                </div>
-                                <div>
-                                    넘어지는 사람을 신속하게 감지하고 즉시
-                                    대응함으로써,
-                                </div>
-                                <div>일상 속의 위험을 최소화합니다.</div>
-                                <div>
-                                    더 이상 걱정하지 않고 편안하게 생활할 수
-                                    있는
-                                </div>
-                                <div>안전한 환경을 제공해드립니다.</div>
-                            </Text>
-
-                            <LocateButton>
-                                <Link to="/Login">
-                                    <StartButton>시작하기</StartButton>
-                                </Link>
-                            </LocateButton>
-                        </LeftFirstSection>
-                        <RightFirstSection>
-                            <CAIImg src={CAI} />
-                        </RightFirstSection>
-                    </FirstSection>
-                    <></>
+                    <FirstSection />
+                    <SecondSection />
                 </OnBoardingSectionContainer>
             </OnBoardingWrapper>
         </OnBoardingContainer>
@@ -68,68 +39,5 @@ const OnBoardingWrapper = styled.div`
 const OnBoardingSectionContainer = styled.div`
     width: 100%;
     height: 100%;
-`
-
-const FirstSection = styled.div`
-    padding-top: 6.2vh;
-    display: flex;
-    width: 100%;
-    height: calc(90% - 6.2vh);
-    background: linear-gradient(180deg, rgba(99, 232, 135, 0) 0%, #14dd4c 100%);
-    flex-direction: row;
-
-    align-items: center;
-`
-
-const LeftFirstSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    gap: 45px;
-    width: 50%;
-    padding-left: 11.5vw;
-    align-items: center;
-`
-
-const RightFirstSection = styled.div`
-    width: 50%;
-    height: 100%;
-    display: flex;
-    padding-right: 11.5vw;
-    align-items: center;
-`
-
-const StartButton = styled.button`
-    width: 10.625rem;
-    height: 3rem;
-    padding: 0;
-    padding: 0;
-    border-radius: 50px;
-    border: none;
-    background-color: #ff7283;
-`
-
-const CAIImg = styled.img`
-    width: 33.5vw;
-    height: 33.5vw;
-`
-
-const Title = styled.span`
-    width: 100%;
-    display: flex;
-    flex-direction: flex-end;
-    font-size: 2.2rem;
-`
-
-const LocateButton = styled.span`
-    width: 100%;
-    display: flex;
-    flex-direction: flex-end;
-`
-
-const Text = styled.span`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
 `
 export default Main
